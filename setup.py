1de0a878708ca48e1b0060a0ffcac16e6ec04c25
Ben Sandler <ben1sandler@gmail.com>
2016-09-03 18:25:33 -0400

    Note python3 support in setup.py

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
