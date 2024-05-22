a47408d65cd25546c79c81adde444663fa0ad9d7
Brian Grohe <grohe43@gmail.com>
2024-05-22 15:12:26 -0400

    Add redis-server to devcontainer for local running of tests to pass (#37)
    Got tests to pass when running in devcontainer
    
    These commands get around rc policy that prevents the redis server from starting. I also upgraded the image to 3.9 because the command does not work on python 3.8

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+printf '#!/bin/sh\nexit 0' > /usr/sbin/policy-rc.d
+sudo apt-get update && sudo apt-get install -y redis-server
 set -e
 python3 -m venv --upgrade-deps .venv
 . .venv/bin/activate
